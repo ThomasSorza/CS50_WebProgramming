47e6ef919542963122404dd2740a95ce2fc14caa
ThomasSorza <thomas@sorza.com>
2022-04-11 12:55:56 -0500

    Added Lambda function files

--- Python/functionalProgramming/decorators.py
+++ Python/functionalProgramming/decorators.py
@@ -1,6 +1,6 @@
 def announce(f):
     def wrapper():#takes a function and add more key habilities
-        print("About to yun te function...")
+        print("About to run te function...")
         f()
         print("Done with the function.")
     return wrapper
---
+++ Python/functionalProgramming/lambdaFunc.py
@@ -0,0 +1,6 @@
+from numpy import square
+
+
+squareSum = lambda x: x * x + x
+x = int(input('Enter a number:'))
+print (squareSum(x))
