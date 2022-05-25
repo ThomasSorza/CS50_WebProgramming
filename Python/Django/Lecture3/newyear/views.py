c987a0693932e7445e46099dea63a1d8ce8ed6ce
ThomasSorza <thomas@sorza.com>
2022-05-25 01:17:07 -0500

    Edited condition for newyear app and created static Style directories

--- Python/Django/Lecture3/newyear/views.py
+++ Python/Django/Lecture3/newyear/views.py
@@ -6,5 +6,6 @@ from django.shortcuts import render
 def index(request):
     now = datetime.datetime.now()
     return render(request, "newyear/index.html", {
-        "newyear": now.month == 1 and now.day == 1
+       # "newyear": now.month == 1 and now.day == 1
+       "newyear": True
     })
